bdfce3e893ae0ed0be19b1c12ab8beb72c6ad2e9
Christopher Chin <=>
2017-10-28 18:26:01 -0400

    Basic listener.

--- src/main.rs
+++ src/main.rs
@@ -5,11 +5,62 @@ use clap::App;
 #[macro_use]
 extern crate error_chain;
 
+extern crate serde_json;
+
 mod result;
 use result::*;
 
-fn run() -> Result<()> {
+use std::io::Read;
+use std::net::{TcpListener, TcpStream};
+use serde_json::Value;
+
+const DEFAULT_PORT: &'static str = "12345";
+const BUF_SIZE: usize = 1024;
+
+/// Each connection is considered a program.
+fn handle_client(stream: &mut TcpStream) -> Result<()> {
+    loop {
+        let mut buffer = [ 0; BUF_SIZE ];
+
+        // TODO: Messages greater than buffer size are truncated and will cause a JSON error
+        let read_size = stream.read(&mut buffer).chain_err(|| "Failed to read stream.")?;
+        if read_size == 0 {
+            // Either zero sized message or EOF
+            break;
+        }
+
+        let read_str = String::from(std::str::from_utf8(&buffer).chain_err(|| "Failed to parse utf-8.")?);
+        let read_str = read_str.trim();
+
+        println!("{:?}", read_str);
+        println!("{}", read_str);
+
+        //let v: Value = serde_json::from_str(&read_str).chain_err(|| "Failed to parse JSON.")?;
+
+        //println!("VALUE: {:?}", v["key"]);
+    }
+
+
+    println!("Connection closed.");
+    Ok(())
+}
+
+fn run(args: u16) -> Result<()> {
     println!("Running!");
+
+    println!("Listening on port {}", args);
+    let listener = TcpListener::bind(format!("127.0.0.1:{}", args)).unwrap();
+
+    for stream in listener.incoming() {
+        let mut stream = stream.chain_err(|| "Failed to open stream.")?;
+        match handle_client(&mut stream).chain_err(|| "Failed handling stream.") {
+            Ok(_) => {},
+            Err(e) => {
+                println!(" Failed to handle connection: {}", e);
+            }
+        }
+    }
+
     Ok(())
 }
 
@@ -17,7 +68,13 @@ fn main() {
     let yaml = load_yaml!("cli.yml");
     let matches = App::from_yaml(yaml).get_matches();
 
-    if let Err(ref e) = run() {
+    let listen_port = matches.value_of("port")
+                             .unwrap_or(DEFAULT_PORT)
+                             .parse::<u16>()
+                             .expect("Invalid port");
+
+
+    if let Err(ref e) = run(listen_port) {
         use std::io::Write;
         let stderr = &mut ::std::io::stderr();
         let msg = "Error writing to stderr";
