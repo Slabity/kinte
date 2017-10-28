23e40ed92590bfa94225a3ad1c9c49b3648b9e8b
Tyler Slabinski <tslabinski@slabity.net>
2017-10-28 15:28:48 -0400

    Add error chain setup

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,34 @@
 extern crate clap;
 use clap::App;
 
+#[macro_use]
+extern crate error_chain;
+
+mod result;
+use result::*;
+
+fn run() -> Result<()> {
+    println!("Running!");
+    Ok(())
+}
+
 fn main() {
     let yaml = load_yaml!("cli.yml");
     let matches = App::from_yaml(yaml).get_matches();
+
+    if let Err(ref e) = run() {
+        use std::io::Write;
+        let stderr = &mut ::std::io::stderr();
+        let msg = "Error writing to stderr";
+
+        writeln!(stderr, "error: {}", e).expect(msg);
+
+        for e in e.iter().skip(1) {
+            writeln!(stderr, "caused by: {}", e).expect(msg);
+        }
+
+        if let Some(backtrace) = e.backtrace() {
+            writeln!(stderr, "backtrace: {:?}", backtrace).expect(msg);
+        }
+    }
 }
